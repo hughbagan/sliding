9fce6d51428ca0028f0dfe4120d75b85849863a4
hughbagan <25750667+hughbagan@users.noreply.github.com>
2020-08-26 18:43:51 -0600

    Adjusted Battle background tween and actors' ready position

--- src/battle/Battle.gd
+++ src/battle/Battle.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var center :Vector2 = $Center.get_position()
-var oval_radius := Vector2(270.0, 15.0) # the ellipse that the actors move along
+var oval_radius := Vector2(230.0, 15.0) # the ellipse that the actors move along
 onready var actors := [$YSort/Char1, $YSort/Enemy]
 onready var actors_min := Vector2(center.x - oval_radius.x, center.y - oval_radius.y)
 onready var actors_max := Vector2(center.x + oval_radius.x, center.y + oval_radius.y)
@@ -9,18 +9,26 @@ var actor_x_offset := 0.1
 
 var tween_x := []
 var tweening := false
-var tween_speed := 1.7 # in seconds
+var tween_speed := 1.0 # in seconds
 
 var first_pos = [0.65, 1.65]	# Players in front, Enemy in behind
-var second_pos = [1.3, 0.3] # Players behind, Enemy in front
+var second_pos = [1.3, 0.3] 	# Players behind, Enemy in front
 
 
 func _ready():
 	# Initialize
 	for i in range(actors.size()):
-		tween_x.append(0.0)
+		# Set positions for the opening tween
+		if actors[i].get_name() == "Enemy":
+			tween_x.append(1.0)
+			actors[i].x = 1.0
+		else:
+			tween_x.append(0.2)
+			actors[i].x = 0.2
 	actors_set_position(first_pos, true)
 	$Background.set_offset(Vector2(0.0, 0.0))
+	$BackgroundTween.interpolate_property($Background, "offset", $Background.offset, Vector2(-480.0, 0.0), tween_speed, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$BackgroundTween.start()
 
 
 func _physics_process(delta):
@@ -28,24 +36,34 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		for i in range(actors.size()):
 			actors[i].x -= 0.01
-			$Background.set_offset(Vector2($Background.get_offset().x + 2, 0))
+			$Background.set_offset(Vector2($Background.get_offset().x + 5, 0))
 	elif Input.is_action_pressed("ui_right"):
 		for i in range(actors.size()):
 			actors[i].x += 0.01
-			$Background.set_offset(Vector2($Background.get_offset().x - 2, 0))
+			$Background.set_offset(Vector2($Background.get_offset().x - 5, 0))
 	elif Input.is_action_just_pressed("1"):
 		actors_set_position(first_pos, true)
-		$BackgroundTween.interpolate_property($Background, "offset", $Background.offset, Vector2(0.0, 0.0), tween_speed, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$BackgroundTween.interpolate_property($Background, "offset", $Background.offset, Vector2(-480.0, 0.0), tween_speed, Tween.TRANS_QUAD, Tween.EASE_OUT)
 		$BackgroundTween.start()
 	elif Input.is_action_just_pressed("2"):
 		actors_set_position(second_pos, true)
-		$BackgroundTween.interpolate_property($Background, "offset", $Background.offset, Vector2(-500.0, 0.0), tween_speed, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$BackgroundTween.interpolate_property($Background, "offset", $Background.offset, Vector2(-1140.0, 0.0), tween_speed, Tween.TRANS_QUAD, Tween.EASE_OUT)
 		$BackgroundTween.start()
 	
 	# Run the x vars through the trig expressions to get actual position coordinates.
 	# This is crucial for allowing the x vars tweening to work.
 	for i in range(actors.size()):
 		actors[i].set_position(oval_position(oval_fraction_x(actors[i].x), oval_fraction_y(actors[i].x)))
+
+
+func oval_position(x:float, y:float) -> Vector2:
+	"""
+	Calculates a position based on output from oval_fraction (ie. a fraction 
+	of the oval's height and width)
+	"""
+	var pos_x : float = actors_min.x + x * oval_radius.x
+	var pos_y : float = actors_min.y + y * oval_radius.y
+	return Vector2(pos_x, pos_y)
 
 
 func oval_fraction_x(x:float) -> float:
@@ -60,16 +78,6 @@ func oval_fraction_y(x:float) -> float:
 	Calculates a decimal that represents a fraction of the oval's radius height (y).
 	"""
 	return sin(PI*x)
-
-
-func oval_position(x:float, y:float) -> Vector2:
-	"""
-	Calculates a position based on output from oval_fraction (ie. a fraction 
-	of the oval's height and width)
-	"""
-	var pos_x : float = actors_min.x + x * oval_radius.x
-	var pos_y : float = actors_min.y + y * oval_radius.y
-	return Vector2(pos_x, pos_y)
 
 
 func actors_set_position(pos:Array, tween:bool=false):
